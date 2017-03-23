942e1525feaff2e17251024d44297fc9526b2063
Fanny-Jan <fanny.jan12@gmail.com>
2017-03-23 09:43:30 +0100

    ajout fichiers css pour le template

--- footer.php
+++ footer.php
@@ -1,39 +1,39 @@
 <!-- footer -->
 
 
-            <footer>
-                <div class="copyright">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <p>© 2017 - Tous Droits réservés </p>
-                            </div>
-                            <div class="col-md-6">
-                                <ul class="bottom_ul">
-                                    <li><a href="#" class="contact-master" data-toggle="modal" data-target="#contactModal">Contacter Développeur</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-
-            <!-- Modal -->
-            <div class="modal fade" id="contactModal" tabindex="-1" aria-labelledby="contactModal">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h3 class="titre-modal">Contacter le webmaster</h3>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Team K<br/>Lab'O<br/>1, avenue du Champ de Mars<br/>45000 Orléans<br/>06 00 00 00 00</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!--            <footer>-->
+<!--                <div class="copyright">-->
+<!--                    <div class="container">-->
+<!--                        <div class="row">-->
+<!--                            <div class="col-md-6">-->
+<!--                                <p>© 2017 - Tous Droits réservés </p>-->
+<!--                            </div>-->
+<!--                            <div class="col-md-6">-->
+<!--                                <ul class="bottom_ul">-->
+<!--                                    <li><a href="#" class="contact-master" data-toggle="modal" data-target="#contactModal">Contacter Développeur</a></li>-->
+<!--                                </ul>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </footer>-->
+<!---->
+<!--            <!-- Modal -->-->
+<!--            <div class="modal fade" id="contactModal" tabindex="-1" aria-labelledby="contactModal">-->
+<!--                <div class="modal-dialog" role="document">-->
+<!--                    <div class="modal-content">-->
+<!--                        <div class="modal-header">-->
+<!--                            <h3 class="titre-modal">Contacter le webmaster</h3>-->
+<!--                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">-->
+<!--                                <span aria-hidden="true">&times;</span>-->
+<!--                            </button>-->
+<!--                        </div>-->
+<!--                        <div class="modal-body">-->
+<!--                            <p>Team K<br/>Lab'O<br/>1, avenue du Champ de Mars<br/>45000 Orléans<br/>06 00 00 00 00</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
 
             <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
