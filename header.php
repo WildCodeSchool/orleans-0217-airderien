e31fcd72342093d420d6602e54d12a3bba33a979
Biovor <duriez.ludovic@gmail.com>
2017-03-27 11:40:57 +0200

    gg

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
         <title>air de rien</title>
         <link href="https://fonts.googleapis.com/css?family=Boogaloo|Gloria+Hallelujah|Indie+Flower|Oxygen" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" >
         <link rel="stylesheet" type="text/css" href="css/user.css">
+
 
         <meta charset="utf-8">
     </head>
--- nav.php
+++ nav.php
@@ -8,17 +8,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand  toggle-btn" href="#">Titre Spectacle <span style="font-size:16px;" class="pull-right hidden-xs"></span></a>
+            <a class="navbar-brand  toggle-btn" href="#">Titre Spectacl <span style="font-size:16px;" class="pull-right hidden-xs"></span></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="#">Acceuil<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
-                <li ><a href="#">Les Personnages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
-                <li ><a href="#">Notre Calendrier<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
-                <li><a href="#">Galerie Multimedia<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
-                <li ><a href="#">Contact<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
-                <li ><a href="#">Revue de Presse<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+                <li class="slidenav"><a href="#">Acceuil<span style="font-size: 25px" class="pull-right hidden-xs showopacity fa fa-home"</span></a></li>
+                <li class="slidenav"><a href="#">Les Personnages<span style="font-size: 25px" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+                <li class="slidenav"><a href="#">Notre Calendrier<span style="font-size: 25px" class="pull-right hidden-xs showopacity fa fa-calendar"></span></a></li>
+                <li class="slidenav"><a href="#">Galerie Multimedia<span style="font-size:25px" class="pull-right hidden-xs showopacity fa fa-play-circle"></span></a></li>
+                <li class="slidenav"><a href="#">Contact<span style="font-size:25px" class="pull-right hidden-xs showopacity fa fa-phone-square"></span></a></li>
+                <li class="slidenav"><a href="#">Revue de Presse<span style="font-size:25px" class="pull-right hidden-xs showopacity fa fa-newspaper-o"></span></a></li>
             </ul>
         </div>
     </div>
