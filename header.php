53291b66853d5dabab59d147f905a84a2d26f982
Biovor <duriez.ludovic@gmail.com>
2017-03-27 15:02:48 +0200

    side bar ok

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
         <title>air de rien</title>
         <link href="https://fonts.googleapis.com/css?family=Boogaloo|Gloria+Hallelujah|Indie+Flower|Oxygen" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-icons.css">
         <link rel="stylesheet" type="text/css" href="css/user.css">
 
         <meta charset="utf-8">
