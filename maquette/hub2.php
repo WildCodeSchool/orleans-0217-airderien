1e249835c2a338058fbbd9646f0660eab8ca9112
Biovor <duriez.ludovic@gmail.com>
2017-03-30 17:19:12 +0200

    hub

---
+++ maquette/hub2.php
@@ -0,0 +1,199 @@
+<!DOCTYPE HTML>
+<html lang="fr-FR">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Comme l'air de rien</title>
+
+    <link rel="icon" type="image/png" href="../img/flav.png" />
+    <link href="css/hub/reset.css" media="all" rel="stylesheet" type="text/css">
+    <link href="css/hub/styles.css" media="all" rel="stylesheet" type="text/css">
+
+</head>
+<body class="w100 cbp-spmenu-push">
+
+<!-- ////////////////////   START  PAGE   \\\\\\\\\\\\\\\\\\\\ -->
+<div id="home" class="desktop w100 container">
+    <!-- ////////////////////  START CONTENU  \\\\\\\\\\\\\\\\\\\\ -->
+    <div class="w100 content txtcenter">
+        <!-- ////////////////////   START  LEFT   \\\\\\\\\\\\\\\\\\\\ -->
+        <section id="home-left" class="home-panel w50 h100 left">
+            <!-- >>> BACKGROUND <<< -->
+            <div id="left-background" class="w50 background left-background"></div>
+            <!-- >>> TOP <<< -->
+            <div class="home-title fittext block-1 w100">
+                <div class="wrapper-1">
+                    <div class="home-box">
+                        <h3>Comme</h3>
+                        <h2>l'air</h2>
+                        <h4>de rien</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- >>> OVER <<< -->
+            <div id="home-over-left" class="home-over w50 h100">
+                <div class="home-over-content w100 h100">
+                    <!-- >>> TOP <<< -->
+                    <div class="home-title fittext block-2 w100">
+                        <div class="wrapper-2">
+                            <div class="home-box">
+                                <h3>Comme</h3>
+                                <h2>l'air</h2>
+                                <h4>de rien</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- >>> LOGO <<< -->
+                    <div id="home-logo-left" class="home-logo block-3 w100">
+                        <div class="wrapper-3">
+                            <div class="home-box fittext w60">
+                                <h2>Compagnie</h2>
+                                <h3>Comme l'air de rien</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- >>> MENU <<< -->
+                    <footer class="w60 center block-4 w100">
+                        <div class="wrapper-4">
+                            <div class="home-box w60">
+                                <hr>
+                                <nav id="nav-do" class="nav">
+                                    <ul>
+                                        <li class="txtleft"><a href="/les-ateliers-de-theatre">les ateliers</a></li>
+                                        <li class="txtcenter"><a href="/lieu-de-formation">lieu de formation</a></li>
+                                        <li class="txtright"><a href="/les-intervenants-des-ateliers-theatre">les intervenants</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </footer>
+                    <!-- >>> OPEN LEFT <<< -->
+                    <div id="menu-left" class="buttonset block-5 w100">
+                        <div class="wrapper-5">
+                            <div class="home-box">
+                                <p id="left-button"><a href="#" id="showLeftPush"></a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- \\\\\\\\\\\\\\\\\\\\   END    LEFT   //////////////////// -->
+        <!-- ////////////////////   START RIGHT   \\\\\\\\\\\\\\\\\\\\ -->
+        <section id="home-right" class="home-panel w50 h100 left">
+            <!-- >>> BACKGROUND <<< -->
+            <div id="right-background" class="w50 background right-background"></div>
+            <!-- >>> TOP <<< -->
+            <div class="home-title fittext block-1 w100">
+                <div class="wrapper-1">
+                    <div class="home-box">
+                        <h3>Devine</h3>
+                        <h2>qui vient</h2>
+                        <h4>dîner se soir</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- >>> OVER <<< -->
+            <div id="home-over-right" class="home-over w50 h100">
+                <div class="home-over-content w100 h100">
+                    <!-- >>> TOP <<< -->
+                    <div class="home-title fittext block-2 w100">
+                        <div class="wrapper-2">
+                            <div class="home-box">
+                                <h3>Devine</h3>
+                                <h2>qui vient</h2>
+                                <h4>diner se soir</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- >>> LOGO <<< -->
+                    <div id="home-logo-right" class="home-logo block-3 w100">
+                        <div class="wrapper-3">
+                            <div class="home-box fittext w60">
+                                <h2>Nos</h2>
+                                <h3>Spectacles</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- >>> MENU <<< -->
+                    <footer class="w60 center block-4 w100">
+                        <div class="wrapper-4">
+                            <div class="home-box w60">
+                                <hr>
+                                <nav id="nav-see" class="nav">
+                                    <ul>
+                                        <li class="txtleft"><a href="/la-compagnie">la compagnie</a></li>
+                                        <li class="txtcenter"><a href="/spectacles-tout-public">tout public</a></li>
+                                        <li class="txtright"><a href="/spectacles-jeune-public">jeune public</a></li>
+                                    </ul>
+                                    <div class="clear"></div>
+                                </nav>
+                            </div>
+                        </div>
+                    </footer>
+                    <!-- >>> OPEN RIGHT <<< -->
+                    <div id="menu-right" class="buttonset block-5 w100">
+                        <div class="wrapper-5">
+                            <div class="home-box">
+                                <p id="right-button"><a href="#" id="showRightPush"></a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- \\\\\\\\\\\\\\\\\\\\   END    RIGHT   //////////////////// -->
+        <div class="clear"></div>
+    </div>
+    <!-- \\\\\\\\\\\\\\\\\\\\   END  CONTENU   //////////////////// -->
+</div>
+<!-- \\\\\\\\\\\\\\\\\\\\  END  CONTENEUR  //////////////////// -->
+<div id="home" class="mobile w100 h100">
+    <section id="home-mobile-top" class="w100 h50 block">
+        <div class="home-mobile-logo w50 txtcenter wrapper">
+            <div class="home-box">
+                <h2>Compagnie</h2>
+                <h2>du Petit Matin</h2>
+            </div>
+        </div>
+        <div class="home-mobile-title w50 txtcenter wrapper">
+            <div class="home-box">
+                <h3><a href="/les-ateliers">je veux</a></h3>
+                <h2><a href="/les-ateliers">faire</a></h2>
+                <h4><a href="/les-ateliers">du théâtre</a></h4>
+            </div>
+        </div>
+    </section>
+    <section id="home-mobile-bottom" class="w100 h50 block">
+        <div class="home-mobile-logo w50 txtcenter wrapper">
+            <div class="home-box">
+                <h2>Compagnie</h2>
+                <h2>du Petit Matin</h2>
+            </div>
+        </div>
+        <div class="home-mobile-title w50 txtcenter wrapper">
+            <div class="home-box">
+                <h3><a href="/la-compagnie">je veux</a></h3>
+                <h2><a href="/la-compagnie">voir</a></h2>
+                <h4><a href="/la-compagnie">du théâtre</a></h4>
+            </div>
+        </div>
+    </section>
+</div>
+<script type='text/javascript' src='js/hub/jquery-1.11.1.min.js'></script>
+<script type="text/javascript" src="js/hub/jquery.fittext.js"></script>
+<script type="text/javascript" src="js/hub/jquery.backstretch.min.js"></script>
+<script type="text/javascript" src="js/hub/main.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(".fittext").fitText();
+        $(".left-background").backstretch("images/bd.jpg");
+        $(".right-background").backstretch("images/bg.jpg");
+        $('#home-left .home-over').niceScroll({styler:"fb",cursorcolor:"#7e646f"});
+        $("#home-right .home-over").niceScroll({styler:"fb",cursorcolor:"#fff"});
+        $(".left-side").niceScroll({styler:"fb",cursorcolor:"#7e646f"});
+        $(".right-side").niceScroll({styler:"fb",cursorcolor:"#fff"});
+    });
+</script>
+</body>
+</html>
