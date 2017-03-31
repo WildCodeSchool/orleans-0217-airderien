25f2f044c2821bdd702272dcccb432a1a6846508
Biovor <duriez.ludovic@gmail.com>
2017-03-31 14:00:29 +0200

    bug hub ok

--- hub/js/main.js
+++ hub/js/main.js
@@ -49,8 +49,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $(".fittext").fitText();
-    $(".left-background").backstretch("images/bd.jpg");
-    $(".right-background").backstretch("images/bg.jpg");
+    $(".left-background").backstretch("../maquette/images/bd.jpg");
+    $(".right-background").backstretch("../maquette/images/bg.jpg");
     $('#home-left .home-over').niceScroll({styler:"fb",cursorcolor:"#7e646f"});
     $("#home-right .home-over").niceScroll({styler:"fb",cursorcolor:"#fff"});
     $(".left-side").niceScroll({styler:"fb",cursorcolor:"#7e646f"});
