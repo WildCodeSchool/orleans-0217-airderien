b18e73724989f109de701eff7912c1041a51ae4e
Dylan Bouchet <dylan.bouchet@gmail.com>
2017-04-19 12:47:23 +0200

    ajout partenaires

--- public/admin.php
+++ public/admin.php
@@ -42,6 +42,25 @@ elseif ($route == 'deletePersonnage') {
     $render = $personnage->deletePersonnage();
 }
 
+elseif ($route == 'showPartenaire') {
+    $partenaire = new \air_de_rien\controller\PartenaireController();
+    $render = $partenaire->index();
+}
+
+elseif ($route == 'addPartenaire') {
+    $partenaire = new \air_de_rien\controller\PartenaireController();
+    $render = $partenaire->addPartenaire();
+
+}
+elseif ($route == 'updatePartenaire') {
+    $partenaire = new \air_de_rien\controller\PartenaireController();
+    $render = $partenaire->updatePartenaire($_GET['id']);
+}
+elseif ($route == 'deletePartenaire') {
+    $partenaire = new \air_de_rien\controller\PartenaireController();
+    $render = $partenaire->deletePartenaire();
+}
+
 echo $renderHeader;
 echo $render;
 echo $renderFooter;
---
+++ src/model/PartenaireRequete.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace air_de_rien\model;
+
+use air_de_rien\model\DB;
+
+class PartenaireRequete extends DB
+{
+
+    public function showPartenaire()
+    {
+        return $this->findAll('partenaire');
+    }
+    public function deletePartenaire()
+    {
+        $pdo = new DB();
+        $query = "DELETE FROM partenaire WHERE id=:id" ;
+        $prep = $pdo->db->prepare($query);
+        $prep->bindValue(':id', $_POST['id'], \PDO::PARAM_INT);
+        $prep->execute();
+    }
+
+    public function addPartenaire($postClean)
+    {
+        $pdo = new DB();
+
+        $query = "INSERT INTO partenaire (nomPartenaire, lienLogoPartenaire, lienSitePartenaire) VALUES (:nomPartenaire,:lienLogoPartenaire, :lienSitePartenaire)";
+
+        $prep = $pdo->db->prepare($query);
+        $prep->bindValue(':nomPartenaire', $postClean['nomPartenaire'], \PDO::PARAM_STR);
+        $prep->bindValue(':lienLogoPartenaire', $postClean['lienLogoPartenaire'], \PDO::PARAM_STR);
+        $prep->bindValue(':lienSitePartenaire', $postClean['lienSitePartenaire'], \PDO::PARAM_STR);
+        $prep->execute();
+    }
+
+    public function updatePartenaire($postClean)
+    {
+        $pdo = new DB();
+
+        $query = "UPDATE partenaire SET nomPartenaire=:nomPartenaire,lienLogoPartenaire=:lienLogoPartenaire, 
+        lienSitePartenaire=:lienSitePartenaire WHERE id=:id";
+
+        $prep = $pdo->db->prepare($query);
+        $prep->bindValue(':id', $postClean['id'], \PDO::PARAM_INT);
+        $prep->bindValue(':nomPartenaire', $postClean['nomPartenaire'], \PDO::PARAM_STR);
+        $prep->bindValue(':lienLogoPartenaire', $postClean['lienLogoPartenaire'], \PDO::PARAM_STR);
+        $prep->bindValue(':lienSitePartenaire', $postClean['lienSitePartenaire'], \PDO::PARAM_STR);
+        $prep->execute();
+    }
+}
