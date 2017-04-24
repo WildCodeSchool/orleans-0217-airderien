ba0673f410ad9e012df336af20c88d1895c852d9
Biovor <duriez.ludovic@gmail.com>
2017-04-24 23:01:06 +0200

    ;)

--- src/controller/SpectacleController.php
+++ src/controller/SpectacleController.php
@@ -97,9 +97,14 @@ class SpectacleController extends Controller
         $filter = new SpectacleFilter();
         $form->setInputFilter($filter);
         $check='';
+        $checkhidden='';
         $requete = new SpectacleRequete();
-        if ($_GET['check'] == '1'){
+        if ($_GET['active'] == '1'){
             $check ='checked="checked"';
+        }
+
+        if ($_GET['active'] == '1'){
+            $checkhidden ='checked="checked"';
         }
 
         if (isset($_POST['active']) == '1'){
@@ -126,10 +131,89 @@ class SpectacleController extends Controller
                     'spectacles'=>$spectacles,
                     'typeAction'=>'update',
                     'checked'=>$check,
+                    'checkhidden'=>$checkhidden,
                     'titreButton'=>'Modifier'
                 ]);
     }
 
+    public function doUpdateMembre()
+    {
+        if (!empty($_POST)) {
+            $requete = new MembreRequete();
 
+            $membre = $requete->findOne('membre', $_POST['id']);
+            foreach ($_POST as $key => $val) {
+                $postClean[$key] = htmlentities(trim($val));
+            }
+
+
+            if (!empty($_POST)) {
+                foreach ($_POST as $key => $val) {
+                    $postClean[$key] = trim($val);
+
+                    if (isset($_FILES['photoMembre'])) {
+                        $errors = array();
+                        $file_name = $_FILES['photoMembre']['name'];
+                        $file_tmp = $_FILES['photoMembre']['tmp_name'];
+                        $path_parts = pathinfo($file_name);
+                        $file_ext = $path_parts['extension'];
+                        $newFileName = rand(0, 1000000) . '.' . $file_ext;
+
+                        $extensions = array("jpeg", "jpg", "png");
+
+                        if (in_array($file_ext, $extensions) === false) {
+                            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+
+                        }
+
+                        if (empty($errors)) {
+                            if (file_exists('images/photos/' . $membre->getPhotoMembre())) {
+                                unlink('images/photos/' . $membre->getPhotoMembre());
+                            }
+                            move_uploaded_file($file_tmp, "images/photos/" . $newFileName);
+                            $postClean['photoMembre'] = $newFileName;
+                            echo "Success";
+                        } else {
+                            print_r($errors);
+                        }
+                    }
+                    $requete->updateMembre($postClean);
+                    header('Location:admin.php?route=showMembre');
+                }
+            }
+        }
+    }
+
+    public function updateMembre($id)
+    {
+        $form = new MembreForm();
+        $filter = new MembreFilter();
+        $form->setInputFilter($filter);
+
+        $requete = new MembreRequete();
+        $db = new DB();
+
+        if (!empty($_POST)) {
+            foreach ($_POST as $key => $val) {
+                $postClean[$key] = trim($val);
+            }
+            $requete->updateMembre($postClean);
+            header('Location:admin.php?route=showMembre');
+        }
+
+        $membres = $requete->findAll('membre');
+        $membre = $requete->findOne('membre', $id);
+        $spectacles = $db -> findAll('spectacle');
+
+        return $this->getTwig()
+            ->render('admin/MembreView.html.twig',
+                ['membres'=>$membres,
+                    'form'=>$form,
+                    'membre'=>$membre,
+                    'spectacles'=>$spectacles,
+                    'titreButton'=>'Modifier',
+                    'typeAction'=>'doUpdate'
+                ]);
+    }
 
     }
