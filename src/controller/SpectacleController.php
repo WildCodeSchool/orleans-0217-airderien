9a78237f7af164e0ed0135db464c1f4470fd50fa
Biovor <duriez.ludovic@gmail.com>
2017-04-25 00:06:23 +0200

    choix spectacle ok

--- src/controller/SpectacleController.php
+++ src/controller/SpectacleController.php
@@ -103,14 +103,9 @@ class SpectacleController extends Controller
             $check ='checked="checked"';
         }
 
-        if ($_GET['active'] == '1'){
+        if ($_GET['active'] == '2'){
             $checkhidden ='checked="checked"';
         }
-
-        if (isset($_POST['active']) == '1'){
-            $requete->choixSpectacle();
-        }
-
 
         if (!empty($_POST)) {
             foreach ($_POST as $key => $val) {
@@ -129,19 +124,26 @@ class SpectacleController extends Controller
                 [   'spectacle'=>$spectacle,
                     'form'=>$form,
                     'spectacles'=>$spectacles,
-                    'typeAction'=>'update',
+                    'typeAction'=>'doUpdate',
                     'checked'=>$check,
                     'checkhidden'=>$checkhidden,
                     'titreButton'=>'Modifier'
                 ]);
     }
 
-    public function doUpdateMembre()
+    public function doUpdateSpectacle()
     {
-        if (!empty($_POST)) {
-            $requete = new MembreRequete();
 
-            $membre = $requete->findOne('membre', $_POST['id']);
+        if (!empty($_POST)) {
+            $requete = new SpectacleRequete();
+
+
+            if ($_POST['active'] == '1'){
+                $requete->choixSpectacle();
+            }
+
+
+            $spectacle = $requete->findOne('spectacle', $_POST['id']);
             foreach ($_POST as $key => $val) {
                 $postClean[$key] = htmlentities(trim($val));
             }
@@ -151,10 +153,10 @@ class SpectacleController extends Controller
                 foreach ($_POST as $key => $val) {
                     $postClean[$key] = trim($val);
 
-                    if (isset($_FILES['photoMembre'])) {
+                    if (isset($_FILES['photoSpect'])) {
                         $errors = array();
-                        $file_name = $_FILES['photoMembre']['name'];
-                        $file_tmp = $_FILES['photoMembre']['tmp_name'];
+                        $file_name = $_FILES['photoSpect']['name'];
+                        $file_tmp = $_FILES['photoSpect']['tmp_name'];
                         $path_parts = pathinfo($file_name);
                         $file_ext = $path_parts['extension'];
                         $newFileName = rand(0, 1000000) . '.' . $file_ext;
@@ -167,53 +169,22 @@ class SpectacleController extends Controller
                         }
 
                         if (empty($errors)) {
-                            if (file_exists('images/photos/' . $membre->getPhotoMembre())) {
-                                unlink('images/photos/' . $membre->getPhotoMembre());
+                            if (file_exists('images/photos/' . $spectacle->getPhotoSpect())) {
+                                unlink('images/photos/' . $spectacle->getPhotoSpect());
                             }
                             move_uploaded_file($file_tmp, "images/photos/" . $newFileName);
-                            $postClean['photoMembre'] = $newFileName;
+                            $postClean['photoSpect'] = $newFileName;
                             echo "Success";
                         } else {
                             print_r($errors);
                         }
                     }
-                    $requete->updateMembre($postClean);
-                    header('Location:admin.php?route=showMembre');
+                    $requete->updateSpectacle($postClean);
+                    header('Location:admin.php?route=showSpectacle');
                 }
             }
         }
     }
 
-    public function updateMembre($id)
-    {
-        $form = new MembreForm();
-        $filter = new MembreFilter();
-        $form->setInputFilter($filter);
-
-        $requete = new MembreRequete();
-        $db = new DB();
-
-        if (!empty($_POST)) {
-            foreach ($_POST as $key => $val) {
-                $postClean[$key] = trim($val);
-            }
-            $requete->updateMembre($postClean);
-            header('Location:admin.php?route=showMembre');
-        }
-
-        $membres = $requete->findAll('membre');
-        $membre = $requete->findOne('membre', $id);
-        $spectacles = $db -> findAll('spectacle');
-
-        return $this->getTwig()
-            ->render('admin/MembreView.html.twig',
-                ['membres'=>$membres,
-                    'form'=>$form,
-                    'membre'=>$membre,
-                    'spectacles'=>$spectacles,
-                    'titreButton'=>'Modifier',
-                    'typeAction'=>'doUpdate'
-                ]);
-    }
 
     }
