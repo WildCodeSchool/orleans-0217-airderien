6873c1bb470f648c8e7fb13fc50a8c90cbd9a6a6
Fanny-Jan <fanny.jan12@gmail.com>
2017-04-24 13:26:03 +0200

    envoi de mail re re test

--- src/controller/FooterController.php
+++ src/controller/FooterController.php
@@ -3,6 +3,8 @@
 
 namespace air_de_rien\controller;
 
+require __DIR__ . '/../../vendor/swiftmailer/swiftmailer/lib/swift_required.php';
+
 use air_de_rien\model\CompagnieRequete;
 
 class FooterController extends Controller
@@ -13,28 +15,28 @@ class FooterController extends Controller
      */
     public function footerRender () {
 
-//        if ($_POST['valid_contact']) {
-//
-//            $db = new DB();
-//            $mailAirDeRien = $db -> findAll('compagnie');
-//
-//            $transport = \Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
-//                ->setUsername('MAIL')
-//                ->setPassword('MAIL_PASSWORD')
-//            ;
-//            $mailer = Swift_Mailer::newInstance($transport);
-//
-//            $message = Swift_Message::newInstance('Contact Air de rien')
-//                ->setSubject('Contact Air de rien')
-//                ->setFrom(array($_POST['email'] => $_POST['name']))
-//                ->setReplyTo($_POST['email'])
-//
-//                ->setTo(array($mailAirDeRien->getMailCompagnie() => 'Comme l\'air de rien'))
-//                ->setTo(array(team.wcs.cladr@gmail.com => 'Comme l\'air de rien'))
-//                ->setBody($_POST['message']);
-//
-//            $mailer->send($message);
-//        }
+        if (isset($_POST['contact'])) {
+
+            $db = new DB();
+            $mailAirDeRien = $db -> findAll('compagnie');
+
+            $transport = \Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
+                ->setUsername('MAIL')
+                ->setPassword('MAIL_PASSWORD')
+            ;
+            $mailer = Swift_Mailer::newInstance($transport);
+
+            $message = Swift_Message::newInstance('Contact Air de rien')
+                ->setSubject('Contact Air de rien')
+                ->setFrom(array($_POST['email'] => $_POST['name']))
+                ->setReplyTo($_POST['email'])
+
+                ->setTo(array($mailAirDeRien->getMailCompagnie() => 'Comme l\'air de rien'))
+                ->setTo(array('team.wcs.cladr@gmail.com' => 'Comme l\'air de rien'))
+                ->setBody($_POST['message']);
+
+            $mailer->send($message);
+        }
 
         $compagnieRequete = new CompagnieRequete();
 
